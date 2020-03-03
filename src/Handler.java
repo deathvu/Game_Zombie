c0376e50f8443a49f403086b761f93947eeb64a8
deathvu <andrewkan22@gmail.com>
2020-03-03 13:17:46 +0300

    Fixed thread problem by adding CopyOnWriteArrayList

--- src/Handler.java
+++ src/Handler.java
@@ -1,23 +1,35 @@
 import java.awt.*;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Handler {
 
-    public LinkedList<GameObject> objects = new LinkedList<GameObject>();
+    public CopyOnWriteArrayList<GameObject> objects = new CopyOnWriteArrayList<GameObject>();
+
+
+    public Iterator<GameObject> iterator_toDo() {
+        Iterator <GameObject> it = objects.iterator();
+        return it;
+    }
 
     public void tick() {
-        for (GameObject tmpObj : objects){
-            tmpObj.tick();
+        Iterator<GameObject> it =  iterator_toDo();
+        while (it.hasNext()){
+            it.next().tick();
         }
     }
 
     public void render(Graphics g) {
-        for (GameObject tmpObj : objects){
-            tmpObj.render(g);
+        Iterator<GameObject> it =  iterator_toDo();
+        while (it.hasNext()){
+            it.next().render(g);
         }
     }
 
     public GameObject add(GameObject tmpObj){
+
         objects.add(tmpObj);
         return tmpObj;
     }
